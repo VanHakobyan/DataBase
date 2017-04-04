4a2341fdf5922f3c66b0adb94b03d0e9b0776ef2
VanHakobyan <van19962013@mail.ru>
2017-04-04 21:52:21 +0400

    add

--- FirstEF6App/FirstEF6App/User.cs
+++ FirstEF6App/FirstEF6App/User.cs
@@ -8,8 +8,8 @@ namespace FirstEF6App
 {
     public class User
     {
-        public int Id { get; set; }
         public string Name { get; set; }
+        public int Id { get; set; }
         public int Age { get; set; }
     }
 }
