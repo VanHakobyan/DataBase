a060dea0c67177b92ee503781624fcde4a31401d
VanHakobyan <van19962013@mail.ru>
2017-04-04 19:35:48 +0400

    add server

--- FirstEF6App/FirstEF6App/Program.cs
+++ FirstEF6App/FirstEF6App/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+ 
 namespace FirstEF6App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (UserContext db = new UserContext())
+            {
+               
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Sam", Age = 26 };
+
+               
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("created");
+
+               
+                var users = db.Users;
+                Console.WriteLine("List object");
+                foreach (User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+          
         }
     }
 }
---
+++ FirstEF6App/FirstEF6App/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace FirstEF6App
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
