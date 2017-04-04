25a171c5633f829fc3e7c75db99c3079e82da896
VanHakobyan <van19962013@mail.ru>
2017-04-04 21:19:55 +0400

    add codefirst

---
+++ FirstEF6App/CodeFirst/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class UserContext : DbContext
+    {
+        public UserContext() :
+            base("UserDB")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
