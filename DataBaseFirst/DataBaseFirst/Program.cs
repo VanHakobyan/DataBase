6fb99ba2871cadb4495fd06f0b8c2b899be0854b
VanHakobyan <van19962013@mail.ru>
2017-04-05 19:37:36 +0400

    add example DataBaseFirst

---
+++ DataBaseFirst/DataBaseFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Database2Entities();
+            foreach (var item in context.People)
+            {
+                Console.WriteLine($"{item.Id}: {item.Name} --- {item.Age}");
+            }
+        }
+    }
+}
