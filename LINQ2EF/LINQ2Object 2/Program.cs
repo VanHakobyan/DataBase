2ffc45a271a7f583d7f53d3248dc242ab0b99620
VanHakobyan <van19962013@mail.ru>
2017-04-09 15:37:31 +0400

    add Employe salary orderBy

--- LINQ2EF/LINQ2Object 2/Program.cs
+++ LINQ2EF/LINQ2Object 2/Program.cs
@@ -10,6 +10,20 @@ namespace LINQ2Object_2
     {
         static void Main(string[] args)
         {
+            var employes = new List<Employee>
+            {
+                new Employee{Name="Kara", Age=27, Salary=200000 ,StartDate=DateTime.Parse("10/01/2015") },
+                new Employee{Name="Tina", Age=19, Salary=140000 ,StartDate=DateTime.Parse("20/02/2014") },
+                new Employee{Name="Emmy", Age=39, Salary=800000 ,StartDate=DateTime.Parse("13/03/2015") },
+                new Employee{Name="Ledy Gaga", Age=27, Salary=1200000 ,StartDate=DateTime.Parse("30/11/2016") }
+            };
+            var topSalary = from employee in employes
+                            where employee.Salary > 100000
+                            select employee;
+            foreach (var item in topSalary)
+            {
+                Console.WriteLine($"Name - {item.Name}\n Age - {item.Age} \n Start Date - {item.StartDate} ");
+            }
         }
     }
 }
