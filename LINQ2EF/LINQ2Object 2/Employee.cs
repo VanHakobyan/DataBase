4812642bea915b6e5de3b7e65f6207f0024a1355
VanHakobyan <van19962013@mail.ru>
2017-04-09 15:24:21 +0400

    add employee class

---
+++ LINQ2EF/LINQ2Object 2/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ2Object_2
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
