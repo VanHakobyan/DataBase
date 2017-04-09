881ec2bb11d51880cfc82653e0740af647583d47
VanHakobyan <van19962013@mail.ru>
2017-04-09 15:20:26 +0400

    add linq query

--- LINQ2EF/LINQ2Objects/Program.cs
+++ LINQ2EF/LINQ2Objects/Program.cs
@@ -10,6 +10,23 @@ namespace LINQ2Objects
     {
         static void Main(string[] args)
         {
+            var people = new List<Person>
+            {
+                new Person {Name="Poghos",Age=20},
+                new Person {Name="Petros",Age=23},
+                new Person {Name="John",Age=35},
+                new Person {Name="Lilit",Age=63},
+                new Person {Name="Ani",Age=14}
+            };
+
+            var sortPeopleNames = from person in people
+                                  where person.Age > 25
+                                  select person.Name;
+            foreach (var item in sortPeopleNames)
+            {
+                Console.WriteLine($"Name: {item}");
+            }
+
         }
     }
 }
