baf127f7980ede4ca48c1656b002d8b8e9bea9ef
Vanik Hakobyan <vanhakobyan1996@gmail.com>
2017-12-27 21:58:58 +0400

    Update Toms2.sql

--- YSUpractise/Toms2.sql
+++ YSUpractise/Toms2.sql
@@ -15,3 +15,29 @@ where B.B_DATETIME in
 select max(B_DATETIME) as b from utB group by B_V_ID
 )
 -------------------------------------------------------
+
+three---Mane
+--------------------------------------------
+create database BaseForWork
+use BaseForWork --es karas chgres
+
+create table Transactionn(
+ code int primary key not null,
+ [account] char(12),
+ [type] char,
+ Amount money)
+
+alter table Transactionn --(code, [account], Amount) petq chi
+add constraint myy  check ( 0 < Amount and Amount < 10000000)
+
+insert into Transactionn (code,account,Amount)-- nshi inchnes uzum avelacnel
+ values 
+ (1131, 'depet', 5000),
+ (1125, 'credit', 6000)
+
+select * from Transactionn
+delete from Transactionn where code=1131
+
+select * from Transactionn
+drop table Transactionn
+-------------------------------------------------------
